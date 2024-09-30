292d9ae3beaffa2a1f49968c482dbf841c4add14
William Alexander Matallana <wmatallana28@gmail.com>
2024-09-30 10:46:12 -0500

    Suma de 2 enteros

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hola");
+
+        int n1=5;
+        int n2 = 10;
+
+        int suma = n1+n2;
+
+        System.out.println("LA SUMA ES: " + suma);
+
+
+
+
+
+
+    }
+}
